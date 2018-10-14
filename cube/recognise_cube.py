fa6b56d4a9a5a896ba0f0822d075f9c47a0ce587
Jonathan Loos <jonathan.loos@web.de>
2018-10-14 21:02:25 +0200

    Add color determination WIP

--- cube/recognise_cube.py
+++ cube/recognise_cube.py
@@ -1,9 +1,12 @@
 import imutils
-from imutils.perspective import four_point_transform
 from imutils.perspective import order_points
 import cv2
 import numpy as np
 import math
+
+color_spaces_hsv = [((100, 131, 0), (125, 255, 255))  # blue
+                    ]
+colors = {"blue": (0, 0, 255)}
 
 
 def transform_according_to_reference_square(image, reference_points):
@@ -45,9 +48,18 @@ def transform_according_to_reference_square(image, reference_points):
     return warped
 
 
+def color_for_rect(image, rect):
+    center_rec = get_center_rec(rect, image, rel_margin=0.25)
+    mean_color = np.mean(center_rec, axis=(0, 1))
+    hsv = cv2.cvtColor(mean_color.reshape((1, 1, 3)).astype(np.uint8), cv2.COLOR_BGR2HSV)
+    for j, (lower, upper) in enumerate(color_spaces_hsv):
+        if cv2.inRange(hsv, lower, upper):
+            return j
+    return 6
+
+
 def find_colored_squares_in_image(image):
     image = imutils.resize(image, height=500)
-    print(np.std(image, axis=0))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 50, 100)
@@ -57,18 +69,18 @@ def find_colored_squares_in_image(image):
     if len(rectangles) == 0:
         return image
 
-    warp_ratios = get_warp_ratios(rectangles)
-
-    warp_ratio = np.mean(warp_ratios)
-    if False and warp_ratio > 0.1:
-        ref_index = np.argmin(np.abs(warp_ratios - warp_ratio))
-        reference_rectangle = rectangles[ref_index]
-        image = transform_according_to_reference_square(image, np.reshape(reference_rectangle, (4, 2)))
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (5, 5), 0)
-        edged = cv2.Canny(gray, 50, 100)
-
-        rectangles, _ = get_recs(edged, 0.1)
+    # warp_ratios = get_warp_ratios(rectangles)
+    #
+    # warp_ratio = np.mean(warp_ratios)
+    # if warp_ratio > 0.1:
+    #     ref_index = np.argmin(np.abs(warp_ratios - warp_ratio))
+    #     reference_rectangle = rectangles[ref_index]
+    #     image = transform_according_to_reference_square(image, np.reshape(reference_rectangle, (4, 2)))
+    #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #     gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    #     edged = cv2.Canny(gray, 50, 100)
+    #
+    #     rectangles = get_recs(edged, 0.1)
 
     rectangles = list(rectangles)
 
@@ -81,22 +93,23 @@ def find_colored_squares_in_image(image):
                 rectangles.extend(colored_recs)
                 rectangles = list(remove_doubles(rectangles))
 
-        # green_mask = np.zeros_like(image)
-        # green_mask[:, :, 1] = 100
-        # only_colored = cv2.bitwise_and(green_mask, green_mask, mask=only_colored)
-        # shifted_green = cv2.add(image, only_colored)
-        # # shifted_green[np.where(shifted_green[:, :, 1] < 110)] += np.array([0, 100, 0])
-        # cv2.imshow("shifted", shifted_green)
-
-    # ordered_corners = order_points(np.reshape(approx, (4, 2)))
-    # sq_width = ordered_corners[1][0] - ordered_corners[0][0]
-    # sq_height = ordered_corners[3][1] - ordered_corners[0][1]
-    # print(sq_width, sq_height)
-    # print(ordered_corners[0])
-    # rectangles.append(approx)
     print(f"found {len(rectangles)}")
+    if len(rectangles) > 9:
+        raise AssertionError("Too many squares found")
+
+    recs_reshaped = np.array([order_points(np.reshape(rec, (4, 2))).astype(np.int32) for rec in rectangles])
+    mean_height = int(np.max(recs_reshaped[:, [3, 2], 1] - recs_reshaped[:, [0, 1], 1]))
+    recs_reshaped = sorted(recs_reshaped, key=lambda x: x[0, 0] + mean_height * 5 * (x[0, 1] // mean_height))
+    found_colors = []
+    for i, rec in enumerate(recs_reshaped):
+        x = rec[0, 0]
+        y = rec[0, 1]
+        cv2.putText(image, f"{i+1}", (x + 20, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [0, 0, 255])
+        found_colors.append(color_for_rect(image, rec))
+
+    # rectangles = [rec.reshape((4, 1, 2)) for rec in recs_reshaped]
     cv2.drawContours(image, rectangles, -1, (0, 255, 0), 2)
-    return image
+    return found_colors, image
 
 
 def get_warp_ratios(rectangles):
@@ -133,19 +146,11 @@ def get_recs(image_to_extract_from, rel_similarity_threshold=0.05, colored_image
 
             # check for similarity in color within approx
             if colored_image is not None:
-                x_min, y_min = np.min(approx, axis=(0, 1))
-                x_max, y_max = np.max(approx, axis=(0, 1))
-                width = x_max - x_min
-                height = y_max - y_min
-                x_min = int(x_min + 0.1 * width)
-                x_max = int(x_max - 0.1 * width)
-                y_min = int(y_min + 0.1 * height)
-                y_max = int(y_max - 0.1 * height)
-                mean_color_std = np.mean(np.std(colored_image[y_min:y_max, x_min:x_max], axis=(0, 1)))
+                image_part = get_center_rec(approx, colored_image)
+                mean_color_std = np.mean(np.std(image_part, axis=(0, 1)))
                 print("std", mean_color_std)
                 if mean_color_std > 15:
                     continue
-                cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
 
             # check for similarity in size
             if len(rectangles) > 0 \
@@ -165,6 +170,20 @@ def get_recs(image_to_extract_from, rel_similarity_threshold=0.05, colored_image
     return rectangles
 
 
+def get_center_rec(approx, image, rel_margin=0.1):
+    reshaped = np.reshape(approx, (4, 2))
+    x_min, y_min = np.min(reshaped, axis=(0))
+    x_max, y_max = np.max(reshaped, axis=(0))
+    width = x_max - x_min
+    height = y_max - y_min
+    x_min = int(x_min + rel_margin * width)
+    x_max = int(x_max - rel_margin * width)
+    y_min = int(y_min + rel_margin * height)
+    y_max = int(y_max - rel_margin * height)
+    image_part = image[y_min:y_max, x_min:x_max]
+    return image_part
+
+
 def remove_doubles(rectangles):
     for i, rec in enumerate(rectangles[:-1]):
         for neighbour in rectangles[i + 1:]:
@@ -180,11 +199,12 @@ def remove_doubles(rectangles):
 
 if __name__ == '__main__':
     images_to_print = [f"cube_1_{i}" for i in range(6)]
-    # images_to_print = ["cube_1_5"]
+    # images_to_print = ["cube_1_4"]
     # images_to_print = [f"cube_1_{i}_warped" for i in range(3, 6)]
     for image_name in images_to_print:
         image = cv2.imread(f"./data/{image_name}.png")
-        image = find_colored_squares_in_image(image)
+        found_colors, image = find_colored_squares_in_image(image)
+        print(f"colors {image_name}:", found_colors)
         cv2.imshow(image_name, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
